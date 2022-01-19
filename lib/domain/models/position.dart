0b9b56e8221ee5ce023e9baa6b01d8527dc2deff
Christina <muza-kris@mail.ru>
2022-01-19 11:25:59 +0300

    add main logic for tapping puzzle tiles

---
+++ lib/domain/models/position.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+
+/// 2-dimensional position model.
+class Position extends Equatable implements Comparable<Position> {
+  const Position({required this.x, required this.y});
+
+  /// The x position.
+  final int x;
+
+  /// The y position.
+  final int y;
+
+  @override
+  List<Object> get props => [x, y];
+
+  /// comparing given positions due to their x and y coordinates
+  @override
+  int compareTo(Position other) {
+    if (y < other.y) {
+      return -1;
+    } else if (y > other.y) {
+      return 1;
+    } else {
+      if (x < other.x) {
+        return -1;
+      } else if (x > other.x) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+}
---
+++ lib/presentation/screens/puzzle_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:skolo_slide_hack/di/injector_provider.dart';
+import 'package:skolo_slide_hack/presentation/widgets/simple_tile_widget.dart';
+import 'package:skolo_slide_hack/domain/states/puzzle_state.dart';
+
+class PuzzlePage extends StatefulWidget {
+  const PuzzlePage({Key? key}) : super(key: key);
+
+  @override
+  State<PuzzlePage> createState() => _PuzzlePageState();
+}
+
+class _PuzzlePageState extends State<PuzzlePage> {
+  final puzzleState = injector<PuzzleState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: Observer(
+            builder: (context) {
+              var tiles = puzzleState.tiles;
+
+              return PuzzleBoard(
+                size: puzzleState.size,
+                tiles: List.generate(
+                  16,
+                  (index) => SimpleTileWidget(
+                    onTap: () => puzzleState.onTileTapped(index),
+                    tile: tiles[index],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Displays the board of the puzzle.
+class PuzzleBoard extends StatelessWidget {
+  const PuzzleBoard({
+    Key? key,
+    required this.size,
+    required this.tiles,
+    this.spacing = 8,
+  }) : super(key: key);
+
+  /// The size of the board.
+  final int size;
+
+  /// The tiles to be displayed on the board.
+  final List<Widget> tiles;
+
+  /// The spacing between each tile from [tiles].
+  final double spacing;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: size,
+      mainAxisSpacing: spacing,
+      crossAxisSpacing: spacing,
+      children: tiles,
+    );
+  }
+}

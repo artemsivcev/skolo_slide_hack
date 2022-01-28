08d9513c85ad9851bbe1e083edcb4e387adf06c0
Artem Sivtsev <artemsivcev@gmail.com>
2022-01-28 11:42:35 +0100

    background added

--- lib/domain/constants/colours.dart
+++ lib/domain/constants/colours.dart
@@ -9,3 +9,7 @@ const Color blueColour = Colors.blue;
 const Color blackColour = Colors.black;
 
 const Color colorsPurpleBluePrimary = Color(0xFF7380E8);
+
+const Color colorsBackgroundMenu = Color(0xFF7479BC);
+const Color colorsBackgroundNewGame = Color(0xFF8D6FBB);
+const Color colorsBackgroundGame = Color(0xFF6A9AB7);

531bf165407bad301960c3f984896f66f70f316e
Daria <ddenisova@skolopendra.com>
2022-02-19 11:06:26 +0300

    shuffle puzzle board refactored

--- lib/presentation/widgets/puzzle_board/puzzle_page.dart
+++ lib/presentation/widgets/puzzle_board/puzzle_page.dart
@@ -60,127 +60,106 @@ class _PuzzlePageState extends State<PuzzlePage> with TickerProviderStateMixin {
       builder: (context) {
         var tiles = puzzleState.tiles;
 
-        final TileAnimationState _animatedTileState =
-            injector<TileAnimationState>();
-        final animationPhase = _animatedTileState.currentAnimationPhase;
-        final AnimationController? animationController;
-
-        if (animationPhase == TileAnimationPhase.SHAFFLE) {
-          animationController =
-              injector<ShuffleAnimationState>().animationController;
-        } else {
-          animationController =
-              injector<StartAnimationState>().startAnimationController;
-        }
-
-        return AnimatedBuilder(
-            animation: animationController!,
-            builder: (_, child) {
-              //todo when other animations come, do if/else or switch
-              // if (animationPhase == TileAnimationPhase.SHAFFLE) {
-              return BoardShuffleAnimator(child: child!);
-              // }
-            },
-            child: SizedBox(
-              width: 300,
-              height: 300,
-              child: PuzzleBoard(
-                size: difficultyState.boardSize,
-                tiles: List.generate(
-                  tiles.length,
-                  (index) => AnimatedTile(
-                    tile: tiles[index],
-                    fraction: index / tiles.length,
-                    onTap: () => puzzleState.onTileTapped(index),
-                  ),
-                ),
+        return SizedBox(
+          width: 300,
+          height: 300,
+          child: PuzzleBoard(
+            size: difficultyState.boardSize,
+            tiles: List.generate(
+              tiles.length,
+              (index) => AnimatedTile(
+                tile: tiles[index],
+                fraction: index / tiles.length,
+                onTap: () => puzzleState.onTileTapped(index),
               ),
-            ));
-
-        final showCorrectOrder = !startAnimationState.isStartAnimPart1End;
-
-        // var tiles =
-        //     showCorrectOrder ? puzzleState.correctTiles : puzzleState.tiles;
-        var isCompleted = puzzleState.isComplete;
-
-        final startFlipAnimation = showCorrectOrder
-            ? startAnimationState.flipAnimationPart1
-            : startAnimationState.flipAnimationPart2;
-
-        return Column(
-          children: [
-            const MovementsCounter(),
-            AnimatedBuilder(
-                animation: shuffleAnimationState.shakeAnimation!,
-                child: AnimatedContainer(
-                  width: isCompleted ? 338 : 310,
-                  height: isCompleted ? 338 : 310,
-                  duration: animationOneSecondDuration,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: SizedBox(
-                      width: 300,
-                      height: 300,
-                      child: PuzzleBoard(
-                        size: difficultyState.boardSize,
-                        spacing: 0,
-                        tiles: List.generate(
-                          tiles.length,
-                          (index) => AnimatedBuilder(
-                            animation: startFlipAnimation,
-                            builder: (context, builderChild) {
-                              return Transform(
-                                alignment: Alignment.center,
-                                transform: Matrix4.rotationX(
-                                    startAnimationState.isStartAnimPart2End
-                                        ? 0.0
-                                        : startFlipAnimation.value!),
-                                child: builderChild,
-                              );
-                            },
-                            child: AnimatedPadding(
-                              duration: animationOneSecondDuration,
-                              padding: EdgeInsets.all(
-                                  shuffleAnimationState.shuffled
-                                      ? shuffleAnimationState
-                                          .appearDisappearAnimation!.value!
-                                      : startAnimationState
-                                                  .startAnimationController!
-                                                  .status ==
-                                              AnimationStatus.completed
-                                          ? winAnimationState.spacingValue
-                                          : 0.0),
-                              child: SimpleTileWidget(
-                                tweenStart: index / tiles.length,
-                                tween: winAnimationState.tweenForFlipping,
-                                fadeAnimation: winAnimationState.fadeAnimation!,
-                                isComplete: isCompleted &&
-                                    winAnimationState.isAnimCompleted,
-                                onTap: () => puzzleState.onTileTapped(index),
-                                tile: tiles[index],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                builder: (context, child) {
-                  final sineValue = sin(3 *
-                      2 *
-                      pi *
-                      shuffleAnimationState.animationController!.value);
-                  return Transform.translate(
-                    // 4. apply a translation as a function of the animation value
-                    offset: Offset(sineValue * 9, 0),
-                    // 5. use the child widget
-                    child: child,
-                  );
-                }),
-            GameTimer(),
-          ],
+            ),
+          ),
         );
+
+        // final showCorrectOrder = !startAnimationState.isStartAnimPart1End;
+        //
+        // // var tiles =
+        // //     showCorrectOrder ? puzzleState.correctTiles : puzzleState.tiles;
+        // var isCompleted = puzzleState.isComplete;
+        //
+        // final startFlipAnimation = showCorrectOrder
+        //     ? startAnimationState.flipAnimationPart1
+        //     : startAnimationState.flipAnimationPart2;
+        //
+        // return Column(
+        //   children: [
+        //     const MovementsCounter(),
+        //     AnimatedBuilder(
+        //         animation: shuffleAnimationState.shakeAnimation!,
+        //         child: AnimatedContainer(
+        //           width: isCompleted ? 338 : 310,
+        //           height: isCompleted ? 338 : 310,
+        //           duration: animationOneSecondDuration,
+        //           child: Padding(
+        //             padding: const EdgeInsets.all(10),
+        //             child: SizedBox(
+        //               width: 300,
+        //               height: 300,
+        //               child: PuzzleBoard(
+        //                 size: difficultyState.boardSize,
+        //                 spacing: 0,
+        //                 tiles: List.generate(
+        //                   tiles.length,
+        //                   (index) => AnimatedBuilder(
+        //                     animation: startFlipAnimation,
+        //                     builder: (context, builderChild) {
+        //                       return Transform(
+        //                         alignment: Alignment.center,
+        //                         transform: Matrix4.rotationX(
+        //                             startAnimationState.isStartAnimPart2End
+        //                                 ? 0.0
+        //                                 : startFlipAnimation.value!),
+        //                         child: builderChild,
+        //                       );
+        //                     },
+        //                     child: AnimatedPadding(
+        //                       duration: animationOneSecondDuration,
+        //                       padding: EdgeInsets.all(
+        //                           shuffleAnimationState.shuffled
+        //                               ? shuffleAnimationState
+        //                                   .appearDisappearAnimation!.value!
+        //                               : startAnimationState
+        //                                           .startAnimationController!
+        //                                           .status ==
+        //                                       AnimationStatus.completed
+        //                                   ? winAnimationState.spacingValue
+        //                                   : 0.0),
+        //                       child: SimpleTileWidget(
+        //                         tweenStart: index / tiles.length,
+        //                         tween: winAnimationState.tweenForFlipping,
+        //                         fadeAnimation: winAnimationState.fadeAnimation!,
+        //                         isComplete: isCompleted &&
+        //                             winAnimationState.isAnimCompleted,
+        //                         onTap: () => puzzleState.onTileTapped(index),
+        //                         tile: tiles[index],
+        //                       ),
+        //                     ),
+        //                   ),
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //         builder: (context, child) {
+        //           final sineValue = sin(3 *
+        //               2 *
+        //               pi *
+        //               shuffleAnimationState.animationController!.value);
+        //           return Transform.translate(
+        //             // 4. apply a translation as a function of the animation value
+        //             offset: Offset(sineValue * 9, 0),
+        //             // 5. use the child widget
+        //             child: child,
+        //           );
+        //         }),
+        //     GameTimer(),
+        //   ],
+        // );
       },
     );
   }
@@ -206,29 +185,49 @@ class PuzzleBoard extends StatelessWidget {
 
   final _startAnimation = injector<StartAnimationState>();
   final winAnimationState = injector<WinAnimationState>();
+  final puzzleState = injector<PuzzleState>();
+  final shuffleAnimationState = injector<ShuffleAnimationState>();
 
   @override
   Widget build(BuildContext context) {
     return Observer(builder: (_) {
       final isStartAnimationEnded = _startAnimation.isStartAnimPart2End;
 
+      final TileAnimationState _animatedTileState =
+          injector<TileAnimationState>();
+      final animationPhase = _animatedTileState.currentAnimationPhase;
+      final AnimationController? animationController;
+
+      if (animationPhase == TileAnimationPhase.SHAFFLE) {
+        animationController =
+            injector<ShuffleAnimationState>().animationController;
+      } else {
+        animationController =
+            injector<StartAnimationState>().startAnimationController;
+      }
+
       return AnimatedBuilder(
-          animation: _startAnimation.startAnimationController!,
-          builder: (_, __) {
-            return GridView.count(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              crossAxisCount: size,
-              mainAxisSpacing: isStartAnimationEnded
-                  ? winAnimationState.spacingValue
-                  : _startAnimation.puzzleBoardAxisPaddingAnimation.value!,
-              crossAxisSpacing: isStartAnimationEnded
-                  ? winAnimationState.spacingValue
-                  : _startAnimation.puzzleBoardAxisPaddingAnimation.value!,
-              children: tiles,
-            );
-          });
+        animation: animationController!,
+        builder: (_, child) {
+          //todo uncomment when start animation for board will be added
+          // if (animationPhase == TileAnimationPhase.SHAFFLE) {
+          return BoardShuffleAnimator(child: child!);
+          // }
+        },
+        child: GridView.count(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          crossAxisCount: size,
+          mainAxisSpacing: isStartAnimationEnded
+              ? winAnimationState.spacingValue
+              : _startAnimation.puzzleBoardAxisPaddingAnimation.value!,
+          crossAxisSpacing: isStartAnimationEnded
+              ? winAnimationState.spacingValue
+              : _startAnimation.puzzleBoardAxisPaddingAnimation.value!,
+          children: tiles,
+        ),
+      );
     });
   }
 }
